2ac14abd90c515cbdc2c498c7903662a47868b35
gui-motas <monjogui@gmail.com>
2025-05-24 15:31:11 -0300

    Initial commit

---
+++ src/main/java/com/xalface/microservices/auth/xAlface_AuthService/XAlfaceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xalface.microservices.auth.xAlface_AuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XAlfaceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XAlfaceAuthServiceApplication.class, args);
+	}
+
+}
